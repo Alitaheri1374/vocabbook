4b3b2dfd945bab2de96f0597efd11ba8cf8b7426
Ali Taheri <taheri.aliTaheri.ali@gmail.com>
2025-04-09 15:36:15 +0330

    ورژن 5

--- lib/layer/presentation/add_vocab/add_vocab_page.dart
+++ lib/layer/presentation/add_vocab/add_vocab_page.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vocabbook/constants/app_const.dart';
 import 'package:vocabbook/layer/domain/bloc/vocab_cubit.dart';
+import 'package:vocabbook/layer/domain/entity/vocab_model.dart';
 import 'package:vocabbook/layer/domain/use_case/vocab_services.dart';
 import 'package:vocabbook/locator.dart';
 
 class AddVocabPage extends StatefulWidget {
-  const AddVocabPage({super.key});
+  final VocabModel? vocabModel;
+  const AddVocabPage({super.key,this.vocabModel});
 
   @override
   State<AddVocabPage> createState() => _AddVocabPageState();
@@ -15,6 +17,21 @@ class AddVocabPage extends StatefulWidget {
 class _AddVocabPageState extends State<AddVocabPage> {
   final TextEditingController wordTxtController=TextEditingController();
   final TextEditingController meaningTxtController=TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if(widget.vocabModel!=null){
+      initField();
+    }
+  }
+
+
+  void initField(){
+      wordTxtController.text=widget.vocabModel?.word??'';
+      meaningTxtController.text=widget.vocabModel?.meaning??'';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +54,7 @@ class _AddVocabPageState extends State<AddVocabPage> {
                 ),
                 Spacer(),
                 ElevatedButton(onPressed: (){
-                  add();
+                  task();
                 },
                     child: Text("add"))
               ],
@@ -45,17 +62,45 @@ class _AddVocabPageState extends State<AddVocabPage> {
           )),
     );
   }
+
+  task(){
+    if(widget.vocabModel!=null){
+      edit();
+    }
+    else{
+      add();
+    }
+  }
+
   add()async{
     VocabServices vocabServices=locator<VocabServices>();
     String word=wordTxtController.text;
     String? meaning=meaningTxtController.text.isNotEmpty?meaningTxtController.text:null;
     bool isInsert=await vocabServices.insert(word: word,meaning: meaning);
     if(isInsert && mounted){
-      BlocProvider.of<VocabCubit>(context).fetchVocab();
-      Navigator.pop(context);
+      changeSuccess();
     }
     else if(mounted){
       debugPrint("error in Insert Data");
     }
   }
+  edit()async{
+    VocabServices vocabServices=locator<VocabServices>();
+    String word=wordTxtController.text;
+    String? meaning=meaningTxtController.text.isNotEmpty?meaningTxtController.text:null;
+    bool isUpdate=await vocabServices.update(id:widget.vocabModel!.id,word: word,meaning: meaning,typeWord: widget.vocabModel?.typeWord,
+      status: widget.vocabModel?.status,isFavorite: widget.vocabModel?.isFavorite,createdTime: widget.vocabModel?.createdTime
+    );
+    if(isUpdate && mounted){
+      changeSuccess();
+    }
+    else if(mounted){
+      debugPrint("error in edit Data");
+    }
+  }
+  void changeSuccess(){
+    BlocProvider.of<VocabCubit>(context).fetchVocab();
+    Navigator.pop(context);
+  }
+
 }
